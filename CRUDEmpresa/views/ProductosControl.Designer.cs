0b86fef21918fe5279162e7cfbf8a691322bf5eb
davrami <david.rami007@gmail.com>
2018-02-04 23:10:17 +0100

    add and delete product

--- CRUDEmpresa/views/ProductosControl.Designer.cs
+++ CRUDEmpresa/views/ProductosControl.Designer.cs
@@ -30,20 +30,20 @@
         {
             this.components = new System.ComponentModel.Container();
             this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.panel1 = new System.Windows.Forms.Panel();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.btnDelete = new System.Windows.Forms.Button();
             this.idproduteDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.producteDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.preuDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.productesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.btnModify = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.Modify = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
-            this.btnModify = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.panel1.SuspendLayout();
@@ -59,41 +59,34 @@
             this.tabControl1.Controls.Add(this.Modify);
             this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(2);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(896, 489);
+            this.tabControl1.Size = new System.Drawing.Size(672, 397);
             this.tabControl1.TabIndex = 0;
-            // 
-            // tabPage2
-            // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 25);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(888, 460);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "New";
-            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // tabPage1
             // 
             this.tabPage1.Controls.Add(this.panel1);
-            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(2);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(888, 460);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(2);
+            this.tabPage1.Size = new System.Drawing.Size(664, 371);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Productos";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.dataGridView1);
             this.panel1.Controls.Add(this.btnModify);
             this.panel1.Controls.Add(this.btnDelete);
-            this.panel1.Controls.Add(this.dataGridView1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Location = new System.Drawing.Point(2, 2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(882, 454);
+            this.panel1.Size = new System.Drawing.Size(660, 367);
             this.panel1.TabIndex = 0;
             // 
             // dataGridView1
@@ -105,88 +98,105 @@
             this.producteDataGridViewTextBoxColumn,
             this.preuDataGridViewTextBoxColumn});
             this.dataGridView1.DataSource = this.productesBindingSource;
-            this.dataGridView1.Location = new System.Drawing.Point(32, 48);
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(352, 150);
-            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.Size = new System.Drawing.Size(660, 150);
+            this.dataGridView1.TabIndex = 2;
             this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
-            this.dataGridView1.DataBindingComplete += new System.Windows.Forms.DataGridViewBindingCompleteEventHandler(this.dataGridView1_DataBindingComplete);
-            // 
-            // btnDelete
-            // 
-            this.btnDelete.Location = new System.Drawing.Point(48, 328);
-            this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(144, 55);
-            this.btnDelete.TabIndex = 1;
-            this.btnDelete.Text = "Delete";
-            this.btnDelete.UseVisualStyleBackColor = true;
-            this.btnDelete.Click += new System.EventHandler(this.button1_Click);
+            this.dataGridView1.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEndEdit);
+            this.dataGridView1.CellValidating += new System.Windows.Forms.DataGridViewCellValidatingEventHandler(this.dataGridView1_CellValidating);
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValueChanged);
+            this.dataGridView1.DataError += new System.Windows.Forms.DataGridViewDataErrorEventHandler(this.dataGridView1_DataError);
+            this.dataGridView1.UserAddedRow += new System.Windows.Forms.DataGridViewRowEventHandler(this.dataGridView1_UserAddedRow);
             // 
             // idproduteDataGridViewTextBoxColumn
             // 
             this.idproduteDataGridViewTextBoxColumn.DataPropertyName = "id_produte";
             this.idproduteDataGridViewTextBoxColumn.HeaderText = "id_produte";
             this.idproduteDataGridViewTextBoxColumn.Name = "idproduteDataGridViewTextBoxColumn";
-            this.idproduteDataGridViewTextBoxColumn.Width = 103;
             // 
             // producteDataGridViewTextBoxColumn
             // 
             this.producteDataGridViewTextBoxColumn.DataPropertyName = "producte";
             this.producteDataGridViewTextBoxColumn.HeaderText = "producte";
             this.producteDataGridViewTextBoxColumn.Name = "producteDataGridViewTextBoxColumn";
-            this.producteDataGridViewTextBoxColumn.Width = 103;
             // 
             // preuDataGridViewTextBoxColumn
             // 
             this.preuDataGridViewTextBoxColumn.DataPropertyName = "preu";
             this.preuDataGridViewTextBoxColumn.HeaderText = "preu";
             this.preuDataGridViewTextBoxColumn.Name = "preuDataGridViewTextBoxColumn";
-            this.preuDataGridViewTextBoxColumn.Width = 103;
             // 
             // productesBindingSource
             // 
             this.productesBindingSource.DataSource = typeof(CRUDEmpresa.EntityFramework.productes);
             // 
-            // Modify
-            // 
-            this.Modify.Controls.Add(this.label1);
-            this.Modify.Location = new System.Drawing.Point(4, 25);
-            this.Modify.Name = "Modify";
-            this.Modify.Padding = new System.Windows.Forms.Padding(3);
-            this.Modify.Size = new System.Drawing.Size(888, 460);
-            this.Modify.TabIndex = 2;
-            this.Modify.Text = "Modify";
-            this.Modify.UseVisualStyleBackColor = true;
-            // 
             // btnModify
             // 
-            this.btnModify.Location = new System.Drawing.Point(240, 328);
+            this.btnModify.Location = new System.Drawing.Point(180, 266);
+            this.btnModify.Margin = new System.Windows.Forms.Padding(2);
             this.btnModify.Name = "btnModify";
-            this.btnModify.Size = new System.Drawing.Size(144, 55);
+            this.btnModify.Size = new System.Drawing.Size(108, 45);
             this.btnModify.TabIndex = 1;
             this.btnModify.Text = "Modify";
             this.btnModify.UseVisualStyleBackColor = true;
             this.btnModify.Click += new System.EventHandler(this.button1_Click);
             // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(36, 266);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(2);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(108, 45);
+            this.btnDelete.TabIndex = 1;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(2);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(2);
+            this.tabPage2.Size = new System.Drawing.Size(664, 371);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "New";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // Modify
+            // 
+            this.Modify.Controls.Add(this.label1);
+            this.Modify.Location = new System.Drawing.Point(4, 22);
+            this.Modify.Margin = new System.Windows.Forms.Padding(2);
+            this.Modify.Name = "Modify";
+            this.Modify.Padding = new System.Windows.Forms.Padding(2);
+            this.Modify.Size = new System.Drawing.Size(664, 371);
+            this.Modify.TabIndex = 2;
+            this.Modify.Text = "Modify";
+            this.Modify.UseVisualStyleBackColor = true;
+            // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(92, 42);
+            this.label1.Location = new System.Drawing.Point(69, 34);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(68, 17);
+            this.label1.Size = new System.Drawing.Size(54, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "NOMBRE";
             // 
             // ProductosControl
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.Controls.Add(this.tabControl1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "ProductosControl";
-            this.Size = new System.Drawing.Size(896, 489);
+            this.Size = new System.Drawing.Size(672, 397);
             this.Load += new System.EventHandler(this.ProductosControl_Load);
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
@@ -205,10 +215,6 @@
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn idproduteDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn producteDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn preuDataGridViewTextBoxColumn;
         private System.Windows.Forms.BindingSource productesBindingSource;
         private System.Windows.Forms.Button btnDelete;
         private System.Windows.Forms.Button btnModify;
@@ -216,5 +222,9 @@
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
         private System.ComponentModel.BackgroundWorker backgroundWorker2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idproduteDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn producteDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn preuDataGridViewTextBoxColumn;
     }
 }
